583d0360f5f0fde7dac5f2e9dc2bd802d6512b0e
徐思捷 <xusj@shanghaitech.edu.cn>
2025-05-26 12:55:25 +0800

    Fix naive test

--- test/main.py
+++ test/main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 from aioconsole import ainput
 
-sys.path.append(str(Path(__file__).parent.parent))
+sys.path.append(str(Path(__file__).parent.parent / "src"))
 
 from utils.zmq_async import Server
 
@@ -53,24 +53,10 @@ class Passenger:
 
 
 async def testing(server: Server):
-    async def is_received_new_message(oldTimeStamp: int, oldServerMessage: str, Msgunprocessed: bool = False) -> tuple:
-        if Msgunprocessed:
-            return True, oldTimeStamp, oldServerMessage
-        else:
-            try:
-                address, message, timestamp = await asyncio.wait_for(server.read(), timeout=0.01)
-                if address == bindedClient:
-                    return True, timestamp, message
-                else:
-                    return False, oldTimeStamp, oldServerMessage
-            except asyncio.TimeoutError:
-                return False, oldTimeStamp, oldServerMessage
-
     ############ Initialize Passengers ############
     passengers = [Passenger(1, 3, "A")]  # There can be many passengers in testcase.
     timeStamp = -1  # default time stamp is -1
     clientMessage = ""  # default received message is ""
-    messageUnprocessed = False  # Used when receiving new message
     count = 0
 
     # Get the first connected client address as the test target
@@ -85,61 +71,55 @@ async def testing(server: Server):
 
     ############ Passenger timed automata ############
     while True:
-        for each_passenger in passengers:
-            result, timeStamp, clientMessage = await is_received_new_message(timeStamp, clientMessage, messageUnprocessed)
-            messageUnprocessed = False
+        address, clientMessage, timeStamp = await server.read()
+        for passenger in passengers:
+            match passenger.state:
+                case PassengerState.IN_ELEVATOR_1_AT_OTHER_FLOOR:
+                    if clientMessage.endswith(f"floor_arrived@{passenger.target_floor}#{passenger.get_elevator_code()}"):
+                        passenger.current_floor = passenger.target_floor
+                        passenger.change_state(PassengerState.IN_ELEVATOR_1_AT_TARGET_FLOOR)
 
-            if result:
-                match each_passenger.state:
-                    case PassengerState.IN_ELEVATOR_1_AT_OTHER_FLOOR:
-                        if clientMessage.endswith(f"floor_arrived@{each_passenger.target_floor}#{each_passenger.get_elevator_code()}"):
-                            each_passenger.current_floor = each_passenger.target_floor
-                            each_passenger.change_state(PassengerState.IN_ELEVATOR_1_AT_TARGET_FLOOR)
+                case PassengerState.IN_ELEVATOR_1_AT_TARGET_FLOOR:
+                    if clientMessage == "door_opened#1":
+                        logger.info(f"Passenger {passenger.name} is leaving the elevator")
+                        passenger.change_state(PassengerState.OUT_ELEVATOR_0_AT_TARGET_FLOOR)
+                        passenger.finished = True
 
-                    case PassengerState.IN_ELEVATOR_1_AT_TARGET_FLOOR:
-                        if clientMessage == "door_opened#1":
-                            logger.info(f"Passenger {each_passenger.name} is leaving the elevator")
-                            each_passenger.change_state(PassengerState.OUT_ELEVATOR_0_AT_TARGET_FLOOR)
-                            each_passenger.finished = True
+                case PassengerState.IN_ELEVATOR_2_AT_OTHER_FLOOR:
+                    # Not exec in this naive testcase
+                    if clientMessage.endswith(f"floor_arrived@{passenger.target_floor}#{passenger.get_elevator_code()}"):
+                        passenger.current_floor = passenger.target_floor
+                        passenger.change_state(PassengerState.IN_ELEVATOR_2_AT_TARGET_FLOOR)
 
-                    case PassengerState.IN_ELEVATOR_2_AT_OTHER_FLOOR:
-                        # Not exec in this naive testcase
-                        if clientMessage.endswith(f"floor_arrived@{each_passenger.target_floor}#{each_passenger.get_elevator_code()}"):
-                            each_passenger.current_floor = each_passenger.target_floor
-                            each_passenger.change_state(PassengerState.IN_ELEVATOR_2_AT_TARGET_FLOOR)
+                case PassengerState.IN_ELEVATOR_2_AT_TARGET_FLOOR:
+                    if clientMessage == "door_opened#2":
+                        logger.info(f"Passenger {passenger.name} is leaving the elevator")
+                        passenger.change_state(PassengerState.OUT_ELEVATOR_0_AT_TARGET_FLOOR)
+                        passenger.finished = True
 
-                    case PassengerState.IN_ELEVATOR_2_AT_TARGET_FLOOR:
-                        if clientMessage == "door_opened#2":
-                            logger.info(f"Passenger {each_passenger.name} is leaving the elevator")
-                            each_passenger.change_state(PassengerState.OUT_ELEVATOR_0_AT_TARGET_FLOOR)
-                            each_passenger.finished = True
+                case PassengerState.OUT_ELEVATOR_0_AT_OTHER_FLOOR:
+                    if clientMessage.startswith(passenger.matching_signal) and passenger.current_floor == passenger.start_floor:
+                        passenger.set_elevator_code(int(clientMessage.split("#")[-1]))
 
-                    case PassengerState.OUT_ELEVATOR_0_AT_OTHER_FLOOR:
-                        if clientMessage.startswith(each_passenger.matching_signal) and each_passenger.current_floor == each_passenger.start_floor:
-                            each_passenger.set_elevator_code(int(clientMessage.split("#")[-1]))
+                    if clientMessage == f"door_opened#{passenger.get_elevator_code()}":
+                        logger.info(f"Passenger {passenger.name} is entering elevator {passenger.get_elevator_code()}")
+                        await asyncio.sleep(1)
+                        if passenger.get_elevator_code() == 1:
+                            passenger.change_state(PassengerState.IN_ELEVATOR_1_AT_OTHER_FLOOR)
+                        elif passenger.get_elevator_code() == 2:
+                            passenger.change_state(PassengerState.IN_ELEVATOR_2_AT_OTHER_FLOOR)
+                        await server.send(bindedClient, f"select_floor@{passenger.target_floor}#{passenger.get_elevator_code()}")
 
-                        if clientMessage == f"door_opened#{each_passenger.get_elevator_code()}":
-                            logger.info(f"Passenger {each_passenger.name} is entering elevator {each_passenger.get_elevator_code()}")
-                            await asyncio.sleep(1)
-                            if each_passenger.get_elevator_code() == 1:
-                                each_passenger.change_state(PassengerState.IN_ELEVATOR_1_AT_OTHER_FLOOR)
-                            elif each_passenger.get_elevator_code() == 2:
-                                each_passenger.change_state(PassengerState.IN_ELEVATOR_2_AT_OTHER_FLOOR)
-                            await server.send(bindedClient, f"select_floor@{each_passenger.target_floor}#{each_passenger.get_elevator_code()}")
-
-                    case PassengerState.OUT_ELEVATOR_0_AT_TARGET_FLOOR:
-                        if each_passenger.is_finished() and not each_passenger.finished_print:
-                            logger.info(f"Passenger {each_passenger.name} has reached the target floor.")
-                            each_passenger.finished_print = True
-                            count += 1
+                case PassengerState.OUT_ELEVATOR_0_AT_TARGET_FLOOR:
+                    if passenger.is_finished() and not passenger.finished_print:
+                        logger.info(f"Passenger {passenger.name} has reached the target floor.")
+                        passenger.finished_print = True
+                        count += 1
 
         if count == len(passengers):
             logger.info("Test passed: All passengers have reached their target floors!")
-            await asyncio.sleep(1)
             await server.send(bindedClient, "reset")
             break
-
-        await asyncio.sleep(0.01)
 
 
 async def main():
